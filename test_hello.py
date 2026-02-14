9c868fe1f30fa77a46fa777204fdb9ff269e6f3f
Ahsan Saleem <124686826+icecoldkill@users.noreply.github.com>
2026-02-14 16:25:00 +0500

    Remove test for more_bye function
    Removed test for the more_bye function.

--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,8 @@
-from hello import more_hello, more_bye
+from hello import more_hello
 
 
 def test_more_hello():
     assert "hello" == more_hello()
 
 
-def test_more_bye():
-    assert "bye" == more_bye()
+
